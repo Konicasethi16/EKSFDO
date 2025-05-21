afeea09ec49d5bfe50f96d55c44a052fac2a5240
Konicasethi16 <167297988+Konicasethi16@users.noreply.github.com>
2025-05-21 15:14:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -156,7 +156,7 @@ variable "tag" {
   type        = string
   description = "The Docker image tag for TFE. Must be explicitly set as no latest tag is available"
   # renovate: datasource=custom.terraform-enterprise depName=terraform-enterprise versioning=loose
-  default = "v202411-2"
+  default = "v202502-1"
 }
 
 variable "docker_registry_username" {
